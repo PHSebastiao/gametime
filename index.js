c011dfd067b43668bb027c4922b3f8ed4ffc2ce9
PHSebastiao <phsilva.sebastiao@gmail.com>
2021-11-11 10:49:55 -0300

    Adição ao handler

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const routes = require('./routes')
-const routesJogos = require('./routesJogos')
+const express = require('express');
+const app = express();
+const port = 3000;
+const routes = require('./routes');
+const routesJogos = require('./routesJogos');
 
 app.use('/assets', express.static('assets'));
 app.use('/images', express.static('images'));
 app.use('/audio', express.static('audio'));
 
-app.use('/', routes)
-app.use('/jogos', routesJogos)
+app.use('/', routes);
+app.use('/jogos', routesJogos);
+
+app.use('/error', (req, res) => {
+    res.sendFile('./error.html', { root: __dirname });
+})
+
+app.use((error, req, res, next) => {
+    console.log("Error Handling Middleware called");
+    console.log('Path: ', req.path);
+    console.error('Error: ', error);
+
+    if (error.type == 'time-out')
+        res.status(408).redirect('/error');
+    else
+        res.status(500).redirect('/error');
+})
 
 app.get('*', function(req, res) {
-    console.error("Erro ao acessar - ", req.path, req.statusCode, req.statusMessage)
-    res.status(404).sendFile('./error.html', { root: __dirname })
+    res.status(404).redirect('/error');
 });
 
 app.listen(process.env.PORT || port,
